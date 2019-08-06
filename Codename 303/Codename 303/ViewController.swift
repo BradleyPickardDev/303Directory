d81ef7278f78b1d432f05baaf24efaf7f1309402
Bradley Pickard <bradleypickarddev@gmail.com>
2019-08-06 14:31:05 -0600

    Initial Commit with new project

---
+++ Codename 303/Codename 303/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codename 303
+//
+//  Created by Bradley Pickard on 8/6/19.
+//  Copyright © 2019 Bradley Pickard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
